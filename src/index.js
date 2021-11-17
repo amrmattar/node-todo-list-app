598c390aa05ed08f6f16ddec7f418ee4227c1168
Amr <amr.webdevelopment@gmail.com>
2021-11-18 00:50:15 +0200

    adding env file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const cors = require('cors')  
 require('./database/mongoose')
-const Task = require('./models/task')
 const taskRouter = require('./routers/task')
 
 const app = express()
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8000
 
 app.use(express.json())
 app.use(cors())
 app.use(taskRouter)
 
-app.listen(port, () => {})
+app.listen(port, () => {
+    console.log("Server is up on port " + port);
+})
